78f33e8b42779cefdfccb7ad911e4771483c1b43
hnomars <h.noma.r.s@gmail.com>
2021-08-29 21:59:14 +0900

    20210829

--- blogpost/admin.py
+++ blogpost/admin.py
@@ -40,41 +40,76 @@ admin.site.register(BlogModel)
 
 @admin.register(StPointNameModel)
 class StPointNameModelAdmin(ImportExportModelAdmin):
-    ordering =['episode']
+    ordering =['id']
     
     resource_class = StPointNameModelResource
-    formats = [base_formats.XLSX]
+    formats = [base_formats.CSV]
 
+    def get_export_filename(self, request, queryset, file_format):
+        filename = "%s.%s" % ("04_St_name",
+                              file_format.get_extension())
+        return filename    
+        
 @admin.register(StPointModel)
 class StPointModelAdmin(ImportExportModelAdmin):
-    ordering =['point_date']
+    ordering =['-point_date']
 
     resource_class = StPointModelResource
-    formats = [base_formats.XLSX]
+    formats = [base_formats.CSV]
 
+    def get_export_filename(self, request, queryset, file_format):
+        filename = "%s.%s" % ("03_St_point",
+                              file_format.get_extension())
+        return filename    
+        
 @admin.register(SRMModel)
 class SRMModelAdmin(ImportExportModelAdmin):
-    ordering =['SRM_date']
+    ordering =['-SRM_date']
 
     resource_class = SRMModelResource
-    formats = [base_formats.XLSX]
+    formats = [base_formats.CSV]
 
+    def get_export_filename(self, request, queryset, file_format):
+        filename = "%s.%s" % ("01_SRM",
+                              file_format.get_extension())
+        return filename    
+        
 @admin.register(SRMOptionModel)
 class SRMOptionModelAdmin(ImportExportModelAdmin):
     resource_class = SRMOptionModelResource
-    formats = [base_formats.XLSX]
+    formats = [base_formats.CSV]
 
+    def get_export_filename(self, request, queryset, file_format):
+        filename = "%s.%s" % ("02_SRMOP",
+                              file_format.get_extension())
+        return filename    
+        
 @admin.register(WordModel)
 class WordModelAdmin(ImportExportModelAdmin):
     resource_class = WordModelResource
-    formats = [base_formats.XLSX]
+    formats = [base_formats.CSV]
 
+    def get_export_filename(self, request, queryset, file_format):
+        filename = "%s.%s" % ("07_Word",
+                              file_format.get_extension())
+        return filename    
+        
 @admin.register(MonitorModel)
 class MonitorModelAdmin(ImportExportModelAdmin):
     resource_class = MonitorModelResource
-    formats = [base_formats.XLSX]
+    formats = [base_formats.CSV]
+
+    def get_export_filename(self, request, queryset, file_format):
+        filename = "%s.%s" % ("05_monitor",
+                              file_format.get_extension())
+        return filename    
 
 @admin.register(ExerciseModel)
 class ExerciseModelAdmin(ImportExportModelAdmin):
     resource_class = ExerciseModelResource
-    formats = [base_formats.XLSX]
+    formats = [base_formats.CSV]
+
+    def get_export_filename(self, request, queryset, file_format):
+        filename = "%s.%s" % ("06_exercise",
+                              file_format.get_extension())
+        return filename    
